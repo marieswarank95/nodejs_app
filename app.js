a195347e4686eca186e3403a2071f01ea5511f19
marieswarank <marieswarank@teezle.com>
2023-06-23 17:26:27 +0530

    updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello World! version 2')
+  res.send('Hello World! version 3 and CI/CD pipeline created by terraform')
 })
 
 app.listen(port, () => {

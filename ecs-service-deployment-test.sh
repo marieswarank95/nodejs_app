1b64bf47a34e3c8b58a23fec2dfd77575d33faba
marieswarank <marieswarank@teezle.com>
2023-06-27 13:39:37 +0530

    testing

--- ecs-service-deployment-test.sh
+++ ecs-service-deployment-test.sh
@@ -9,14 +9,14 @@ echo "creating new revision of the task definition"
 aws ecs register-task-definition --family nodejs-app-task-definition --cli-input-json file://nodejs-app-td-v_${BUILD_NUMBER}.json
 
 # Update the service with the new task definition and desired count
-test1 = 'aws ecs describe-task-definition --task-definition nodejs-app-task-definition'
-test2 = `aws ecs describe-task-definition --task-definition nodejs-app-task-definition | egrep "revision"'
-test3 = `aws ecs describe-task-definition --task-definition nodejs-app-task-definition | egrep "revision" | tr "/" " "'
-test4 = `aws ecs describe-task-definition --task-definition nodejs-app-task-definition | egrep "revision" | tr "/" " " | awk '{print $2}''
-echo ${test1}
-echo ${test2}
-echo ${test3}
-echo ${test4}
+TEST1=`aws ecs describe-task-definition --task-definition nodejs-app-task-definition`
+TEST2=`aws ecs describe-task-definition --task-definition nodejs-app-task-definition | egrep "revision"`
+TEST3=`aws ecs describe-task-definition --task-definition nodejs-app-task-definition | egrep "revision" | tr "/" " "`
+TEST4=`aws ecs describe-task-definition --task-definition nodejs-app-task-definition | egrep "revision" | tr "/" " " | awk '{print $2}'`
+echo ${TEST1}
+echo ${TEST2}
+echo ${TEST3}
+echo ${TEST4}
 TASK_REVISION=`aws ecs describe-task-definition --task-definition nodejs-app-task-definition | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 echo "latest task definition revision is ${TASK_REVISION}"
 #DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
